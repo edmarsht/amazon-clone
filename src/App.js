95ed0a57ed335ca52d629419edb6c24fabba29d3
edmarsht <edtoulet@gmail..com>
2021-09-07 12:32:59 +0200

    work on amazon banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header.js";
+import Home from "./Home.js";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Home Page</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
