f23d45f3d57812e22363093e1f1b7a3bc54f3376
edmarsht <edtoulet@gmail..com>
2021-09-21 02:01:41 +0200

    PaymentPage --> Stripe initialize & CardElements

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import Footer from "./Footer.js";
 import Payment from "./Payment.js";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51ILy9yJc5sg2v9GiNAFtvUEpQXBSGKehgX4BgalIoLWJd37rCb1RSv5RkrC0neygrLBLjmvvCWyrIb3nDkZzHPDn002wkvjS4D"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -17,27 +22,27 @@ function App() {
   useEffect(() => {
     // Will only run once when the app component loeads..
 
-    auth.onAuthStateChanged(authUser => {
-      console.log('THE USER IS >>>', authUser);
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
       if (authUser) {
         // the user just logged in / the user was logged in
-        // if authUser exist, user will be authUser 
+        // if authUser exist, user will be authUser
 
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         // the user is logged out
         // if authUser don'tn exist, user will be null
 
         dispatch({
-          type: 'SET_USER', 
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
@@ -52,7 +57,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
--- src/Payment.js
+++ src/Payment.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct.js";
 import { Link } from "react-router-dom";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
 
 
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
 
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disable, setDisabled] = useState(true);
+
+  const handleSubmit = (e) => {
+    // Do all the stripe stuff
+  };
+
+  const handleChange = (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    // Voit à chaque changement ce qu'il se passe et dit si il y a une erreur de cart epar exemple
+    setDisabled(event.empty);
+    // Désactiver si l'event est empty et donc ne rien faire si c'est vide
+
+    setError(event.error ? event.error.message : "");
+    // Si il y a une erreur ? Montrer l'erreur sinon ne rien faire
+  };
+
   return (
     <div className="payment">
-
       <div className="payment__container">
-
-          <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
 
         <div className="payment__section">
           <div className="payment__title">
@@ -45,13 +69,31 @@ function Payment() {
         </div>
 
         <div className="payment__section">
-            <div className="payment__title">
-                <h3>Payment Method</h3>
-            </div>
-            <div className="payment__details">
-
-            </div>
-
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <p>
+                        Subtotal ({basket.length} items):{" "}
+                        <strong>{` ${value}`}</strong>
+                      </p>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
